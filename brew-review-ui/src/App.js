f06b025a315ac684f5fa2ed9c4be560c27936e1e
Cameron Bowers <cameronbowers0@gmail.com>
2022-02-06 14:57:17 -0800

    Added bootstrap 5 support. Added a draft home page showing beverages. Added react dom and router.

--- brew-review-ui/src/App.js
+++ brew-review-ui/src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import { useState } from 'react';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+
+        <div className="App-header">
+          <Routes>
+            <Route path="/" exact element={<HomePage/>}>
+            </Route>
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ brew-review-ui/src/pages/HomePage.js
@@ -0,0 +1,126 @@
+import { useState, eseEffect, useEffect } from 'react';
+import { useHistory } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { Link } from 'react-router-dom';
+
+function HomePage() {
+    const history = useNavigate();
+
+    return (
+        <>
+            <section class="py-4 my-5">
+                <div class="container ">
+                    <div class="row">
+                        <div class="col-12">
+                            <h2 class="mb-3">List of Beverages</h2>
+                            <button type="button" class="btn btn-success">Create new Beverage</button>
+                            
+                            <p> Tenative logic: For every beverage in beverage table create a card</p>
+                        </div>
+                        
+                        <div class="col-md-6 col-lg-4 mb-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Beverage name</h5>
+
+                                    <ul class="card-text">
+                                        <li>ABV value: ##%</li>
+                                        <li><a href="#">Category name</a></li>
+                                        <li><a href="#">Brewery Name</a></li>
+                                    </ul>
+                                    <div>
+                                    <button href="#" class="btn btn-primary m-2">Read Reviews</button>
+                                    <button type="button" class="btn btn-danger btn-sm m-2">Delete</button>
+                                    <button type="button" class="btn btn-warning btn-sm m-2">Edit</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* Sample cards below. TODO implement card as a componet*/}
+                        <div class="col-md-6 col-lg-4 mb-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Beverage name</h5>
+
+                                    <ul class="card-text">
+                                        <li>ABV value: ##%</li>
+                                        <li><a href="#">Category name</a></li>
+                                        <li><a href="#">Brewery Name</a></li>
+                                    </ul>
+                                    <div>
+                                    <button href="#" class="btn btn-primary m-2">Read Reviews</button>
+                                    <button type="button" class="btn btn-danger btn-sm m-2">Delete</button>
+                                    <button type="button" class="btn btn-warning btn-sm m-2">Edit</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="col-md-6 col-lg-4 mb-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Beverage name</h5>
+
+                                    <ul class="card-text">
+                                        <li>ABV value: ##%</li>
+                                        <li><a href="#">Category name</a></li>
+                                        <li><a href="#">Brewery Name</a></li>
+                                    </ul>
+                                    <div>
+                                    <button href="#" class="btn btn-primary m-2">Read Reviews</button>
+                                    <button type="button" class="btn btn-danger btn-sm m-2">Delete</button>
+                                    <button type="button" class="btn btn-warning btn-sm m-2">Edit</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="col-md-6 col-lg-4 mb-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Beverage name</h5>
+
+                                    <ul class="card-text">
+                                        <li>ABV value: ##%</li>
+                                        <li><a href="#">Category name</a></li>
+                                        <li><a href="#">Brewery Name</a></li>
+                                    </ul>
+                                    <div>
+                                    <button href="#" class="btn btn-primary m-2">Read Reviews</button>
+                                    <button type="button" class="btn btn-danger btn-sm m-2">Delete</button>
+                                    <button type="button" class="btn btn-warning btn-sm m-2">Edit</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="col-md-6 col-lg-4 mb-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Beverage name</h5>
+
+                                    <ul class="card-text">
+                                        <li>ABV value: ##%</li>
+                                        <li><a href="#">Category name</a></li>
+                                        <li><a href="#">Brewery Name</a></li>
+                                    </ul>
+                                    <div>
+                                    <button href="#" class="btn btn-primary m-2">Read Reviews</button>
+                                    <button type="button" class="btn btn-danger btn-sm m-2">Delete</button>
+                                    <button type="button" class="btn btn-warning btn-sm m-2">Edit</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+
+
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default HomePage;
