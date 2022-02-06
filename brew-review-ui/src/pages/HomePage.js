0760159e44c87cc026e333f233f9404ccd27d90f
Cameron Bowers <cameronbowers0@gmail.com>
2022-02-06 15:32:26 -0800

    Added link from home to Add-beverage.

--- brew-review-ui/src/pages/HomePage.js
+++ brew-review-ui/src/pages/HomePage.js
@@ -13,11 +13,13 @@ function HomePage() {
                     <div class="row">
                         <div class="col-12">
                             <h2 class="mb-3">List of Beverages</h2>
-                            <button type="button" class="btn btn-success">Create new Beverage</button>
-                            
+                            <Link to="add-beverage">
+                                <button type="button" class="btn btn-success" href="">Create new Beverage</button>
+                            </Link>
+
                             <p> Tenative logic: For every beverage in beverage table create a card</p>
                         </div>
-                        
+
                         <div class="col-md-6 col-lg-4 mb-4">
                             <div class="card">
                                 <div class="card-body">
@@ -29,9 +31,9 @@ function HomePage() {
                                         <li><a href="#">Brewery Name</a></li>
                                     </ul>
                                     <div>
-                                    <button href="#" class="btn btn-primary m-2">Read Reviews</button>
-                                    <button type="button" class="btn btn-danger btn-sm m-2">Delete</button>
-                                    <button type="button" class="btn btn-warning btn-sm m-2">Edit</button>
+                                        <button href="#" class="btn btn-primary m-2">Read Reviews</button>
+                                        <button type="button" class="btn btn-danger btn-sm m-2">Delete</button>
+                                        <button type="button" class="btn btn-warning btn-sm m-2">Edit</button>
                                     </div>
                                 </div>
                             </div>
@@ -49,14 +51,14 @@ function HomePage() {
                                         <li><a href="#">Brewery Name</a></li>
                                     </ul>
                                     <div>
-                                    <button href="#" class="btn btn-primary m-2">Read Reviews</button>
-                                    <button type="button" class="btn btn-danger btn-sm m-2">Delete</button>
-                                    <button type="button" class="btn btn-warning btn-sm m-2">Edit</button>
+                                        <button href="#" class="btn btn-primary m-2">Read Reviews</button>
+                                        <button type="button" class="btn btn-danger btn-sm m-2">Delete</button>
+                                        <button type="button" class="btn btn-warning btn-sm m-2">Edit</button>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        
+
                         <div class="col-md-6 col-lg-4 mb-4">
                             <div class="card">
                                 <div class="card-body">
@@ -68,14 +70,14 @@ function HomePage() {
                                         <li><a href="#">Brewery Name</a></li>
                                     </ul>
                                     <div>
-                                    <button href="#" class="btn btn-primary m-2">Read Reviews</button>
-                                    <button type="button" class="btn btn-danger btn-sm m-2">Delete</button>
-                                    <button type="button" class="btn btn-warning btn-sm m-2">Edit</button>
+                                        <button href="#" class="btn btn-primary m-2">Read Reviews</button>
+                                        <button type="button" class="btn btn-danger btn-sm m-2">Delete</button>
+                                        <button type="button" class="btn btn-warning btn-sm m-2">Edit</button>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        
+
                         <div class="col-md-6 col-lg-4 mb-4">
                             <div class="card">
                                 <div class="card-body">
@@ -87,14 +89,14 @@ function HomePage() {
                                         <li><a href="#">Brewery Name</a></li>
                                     </ul>
                                     <div>
-                                    <button href="#" class="btn btn-primary m-2">Read Reviews</button>
-                                    <button type="button" class="btn btn-danger btn-sm m-2">Delete</button>
-                                    <button type="button" class="btn btn-warning btn-sm m-2">Edit</button>
+                                        <button href="#" class="btn btn-primary m-2">Read Reviews</button>
+                                        <button type="button" class="btn btn-danger btn-sm m-2">Delete</button>
+                                        <button type="button" class="btn btn-warning btn-sm m-2">Edit</button>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        
+
                         <div class="col-md-6 col-lg-4 mb-4">
                             <div class="card">
                                 <div class="card-body">
@@ -106,14 +108,14 @@ function HomePage() {
                                         <li><a href="#">Brewery Name</a></li>
                                     </ul>
                                     <div>
-                                    <button href="#" class="btn btn-primary m-2">Read Reviews</button>
-                                    <button type="button" class="btn btn-danger btn-sm m-2">Delete</button>
-                                    <button type="button" class="btn btn-warning btn-sm m-2">Edit</button>
+                                        <button href="#" class="btn btn-primary m-2">Read Reviews</button>
+                                        <button type="button" class="btn btn-danger btn-sm m-2">Delete</button>
+                                        <button type="button" class="btn btn-warning btn-sm m-2">Edit</button>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        
+
 
 
                     </div>
